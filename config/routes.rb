ca25dfc7c23d4ae57efb17377c7062504d85246a
tschuste <tschuste@chicagobooth.edu>
2021-07-22 01:46:20 +0000

    Making Progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
   
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 end
